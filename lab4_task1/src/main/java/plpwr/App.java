04bb96c38956d2287a973d66e8b6f53260faaf76
Artur Z <arjamizo@gmail.com>
2014-03-26 03:05:57 +0100

    task 1 xml generator

--- lab4_task1/src/main/java/plpwr/App.java
+++ lab4_task1/src/main/java/plpwr/App.java
@@ -1,13 +1,60 @@
 package plpwr;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    /**
+     * @link http://www.mkyong.com/java/how-to-create-xml-file-in-java-dom/
+     * @param args 
+     */
+    public static void main( String[] args ) 
     {
-        System.out.println( "Hello World!" );
-    }
+	  try {
+ 
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+ 
+		Document doc = docBuilder.newDocument();
+		Element rootElement = doc.createElement("ComputerEngineering");
+		doc.appendChild(rootElement);
+ 
+		Element course = doc.createElement("Course");
+                course.setAttribute("Name", "Application programming - Java and XML technologies");
+                course.setAttribute("Author", "Artur");
+		rootElement.appendChild(course);
+                
+                rootElement.appendChild(doc.createElement("Topics"));
+ 
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(new File("file.xml"));
+                
+		transformer.transform(source, result);
+		transformer.transform(source, new StreamResult(System.out));
+                
+ 
+		System.out.println("\nFile saved!");
+ 
+	  } catch (ParserConfigurationException pce) {
+		pce.printStackTrace();
+	  } catch (TransformerException tfe) {
+		tfe.printStackTrace();
+	  }
+	}
 }
